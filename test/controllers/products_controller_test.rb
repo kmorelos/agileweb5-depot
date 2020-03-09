5cd28314c6f456868d553ef8d18b54970dade4d8
Kia Morelos <kmorelos@bti-usa.com>
2020-03-09 15:45:40 -0600

    created cart object, added a private and placed in concern so that its accessible to all controllers, created relation ships between carts and line items, line items and products, and added cart button.

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -37,13 +37,20 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should update product" do
     patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @title } }
     assert_redirected_to product_url(@product)
+end
+    test "can't delete product in cart" do
+  assert_difference('Product.count', 0) do
+    delete product_url(products(:two))
   end
 
-  test "should destroy product" do
-    assert_difference('Product.count', -1) do
-      delete product_url(@product)
-    end
+  assert_redirected_to products_url
+end
 
-    assert_redirected_to products_url
+test "should destroy product" do
+  assert_difference('Product.count', -1) do
+    delete product_url(@product)
+  end
+
+  assert_redirected_to products_url
   end
 end
